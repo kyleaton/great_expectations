cf14c649bd1c26832f8000919b394dc1eb697599
Aylr <Aylr@users.noreply.github.com>
2020-04-24 17:26:28 -0600

    * marking validation operator commands as deprecating soon

--- great_expectations/cli/validation_operator.py
+++ great_expectations/cli/validation_operator.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 import json
 import sys
 from datetime import datetime
@@ -7,7 +6,7 @@ import click
 
 from great_expectations import DataContext
 from great_expectations import exceptions as ge_exceptions
-from great_expectations.cli.cli_logging import logger
+from great_expectations.cli.mark import Mark as mark
 
 
 from great_expectations.cli.datasource import (
@@ -17,7 +16,6 @@ from great_expectations.cli.datasource import (
 from great_expectations.cli.util import (
     cli_message,
     load_expectation_suite,
-    cli_message_list,
     cli_message_dict
 )
 from great_expectations.core.usage_statistics.usage_statistics import send_usage_message
@@ -30,7 +28,7 @@ except ImportError:
     # We'll redefine this error in code below to catch ProfilerError, which is caught above, so SA errors will
     # just fall through
     SQLAlchemyError = ge_exceptions.ProfilerError
-()
+
 
 @click.group()
 def validation_operator():
@@ -45,6 +43,9 @@ def validation_operator():
     default=None,
     help="The project's great_expectations directory."
 )
+@mark.cli_as_deprecation("""<yellow>In the next major release this command will be deprecated.
+Please consider using `checkpoint list` if you wish to configure a new checkpoint interactively
+  - `checkpoint run` if you wish to run a saved checkpoint</yellow>""")
 def validation_operator_list(directory):
     """List known Validation Operators."""
     try:
@@ -114,6 +115,12 @@ def validation_operator_list(directory):
     "-d",
     default=None,
     help="The project's great_expectations directory.",
+)
+@mark.cli_as_deprecation(
+    """<yellow>In the next major release this command will be deprecated.
+Please consider using either:
+  - `checkpoint new` if you wish to configure a new checkpoint interactively
+  - `checkpoint run` if you wish to run a saved checkpoint</yellow>"""
 )
 def validation_operator_run(name, run_id, validation_config_file, suite, directory):
     # Note though the long lines here aren't pythonic, they look best if Click does the line wraps.
